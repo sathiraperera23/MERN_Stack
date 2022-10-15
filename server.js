4409d3dac6be996c918052f1f75f3ee08e6278f1
Sathira Perera <sathira.perera23@gmail.com>
2022-10-15 10:44:38 +0530

    Added basic views

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(morgan("tiny")); //log requests
 
 app.use(bodyparser.urlencoded({extended:true})) //parse request to body-parser
 
-app.set("view.engine", "ejs") //set view enjine 
+app.set("view engine", "ejs") //set view enjine 
 // app.set("views", path.resolve(__dirname, "views/ejs"))
 
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 // css/style.css
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
-app.use('/css', express.static(path.resolve(__dirname, "assets/js")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
-    res.send("Crud Application");
+    res.render('index');
 })
 
 app.listen(PORT, () => { console.log('Server is running on http://localhost:$(PORT)') });
