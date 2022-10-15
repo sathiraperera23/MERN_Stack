f9c1c12c536e9eecb3a80259a93fb5d7bea8ddf5
Sathira Perera <sathira.perera23@gmail.com>
2022-10-15 10:32:13 +0530

    Preparing structure and server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const exp = require('constants');
+
+const app = express();
+
+dotenv.config({ path: 'config.env' })
+const PORT = process.env.PORT || 8080
+
+app.use(morgan("tiny")); //log requests
+
+app.use(bodyparser.urlencoded({extended:true})) //parse request to body-parser
+
+app.set("view.engine", "ejs") //set view enjine 
+// app.set("views", path.resolve(__dirname, "views/ejs"))
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+// css/style.css
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/css', express.static(path.resolve(__dirname, "assets/js")))
+
+app.get('/', (req, res) => {
+    res.send("Crud Application");
+})
+
+app.listen(PORT, () => { console.log('Server is running on http://localhost:$(PORT)') });
