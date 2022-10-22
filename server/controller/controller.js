9ace4c7c5ce0c7cde0e2426942616a8bb69bec9f
Sathira Perera <sathira.perera23@gmail.com>
2022-10-22 09:10:40 +0530

    Created controller and routes for that

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -2,7 +2,29 @@ var Userdb = require('../model/model');
 
 //create and save new user
 exports.create = (req,res)=>{
+if(!req.body){
+    res.status(400).send({message: "Content cannot be empty!"});
+    return;
+}
+//new user
+const user = new Userdb({
+    name:req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status:req.body.status
 
+})
+//save user in the database
+user.save(user)
+.then(data=>{
+    res.send(data)
+})
+.catch(err=>{
+
+res.status(500).send({
+    message:err.message || "Som error occuerd while creating a create opertaion"
+});
+});
 }
 
 //retrieve and return all users/ retrieve and return a single user
@@ -17,5 +39,5 @@ exports.update = (req, res)=>{
 
 //Delete a user with a specified user id in the request
 exports.delete = (req, res)=>{
-    
+
 }
